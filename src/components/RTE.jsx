c35de057918e525feef958e3207b5d77d813a525
Prince <workwithprince2022@gmail.com>
2024-04-17 17:32:14 +0530

    extension of first commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import conf from "../conf/conf";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
@@ -12,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="0vv55bv282c5uvj9fc41p2nky53hbm8a9777spk92jbhuo66"
+            apiKey={conf.tinymceApiKey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,6 +4,7 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DB_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
 }
 
 export default conf
